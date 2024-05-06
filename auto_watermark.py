a5c8b4474912d676314a47f53d915b331add12db
Bianca Terumi <bia.terumi2000@gmail.com>
2024-05-05 23:01:00 -0300

    feat: excecoes

--- auto_watermark.py
+++ auto_watermark.py
@@ -5,50 +5,54 @@ from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip
 video_extensions = ['.mp4','.avi', '.mkv', '.mov', '.proxy']
 image_extensions = ['.jpg', '.jpeg', '.png']
 
-def directories_validation(video_path, image_path, render_path):
+def create_directories(video_path, image_path, render_path):
 	try:
 		for path in [video_path, image_path, render_path]:
 			if not os.path.exists(path):
 				os.makedirs(path)
 		return True
-	except Exception as e:
+	except OSError as e:
 		print(f"Erro ao criar os diretórios: {str(e)}")
 
 def extension_validation(video, image):
-	"""Verifica se as extensões dos arquivos de vídeo e imagem são suportadas."""
-	video_extension = os.path.splitext(video)[1].lower()
-	image_extension = os.path.splitext(image)[1].lower()
-	if video_extension not in video_extensions:
-		print(f"O formato do vídeo {video} não é suportado.")
-		return False
-	
-	if image_extension not in image_extensions:
-		print(f"O formato da imagem {image} não é suportado.")
-		return False
-	return True
+	try:
+		# Verifica se as extensões dos arquivos de vídeo e imagem são suportadas.
+		video_extension = os.path.splitext(video)[1].lower()
+		image_extension = os.path.splitext(image)[1].lower()
+		if video_extension not in video_extensions:
+			print(f"O formato do vídeo {video} não é suportado.")
+			return False
+		
+		if image_extension not in image_extensions:
+			print(f"O formato da imagem {image} não é suportado.")
+			return False
+		return True
+	except OSError as e:
+		print(f"Erro ao validar as extensões: {str(e)}")
 
 def apply_watermark(video_path, image_path, render_path):
+		# Listando os arquivos nos diretórios de vídeo e imagem
+		video_names = os.listdir(video_path)
+		image_names = os.listdir(image_path)
 
-	# Listando os arquivos nos diretórios de vídeo e imagem
-	video_names = os.listdir(video_path)
-	image_names = os.listdir(image_path)
-
-	# Aplica uma marca d'água em vídeos com imagens correspondentes.
-	for video in video_names:
-		for image in image_names:
-			if extension_validation(video, image):
-				video_file = os.path.join(video_path, video)
-				image_file = os.path.join(image_path, image)
-				video_name = os.path.splitext(video)[0]
-				image_name = os.path.splitext(image)[0]
-
-				if video_name == image_name:
-					video_clip = VideoFileClip(video_file)
-					image_clip = ImageClip(image_file)
-					image_clip = image_clip.set_duration(video_clip.duration)
-					video_with_watermark = CompositeVideoClip([video_clip, image_clip.set_position(('right','top'))])
-					render_path = os.path.join(render_path, f"{video_name}_rendered.mp4")
-					video_with_watermark.write_videofile(render_path, codec='libx264', fps=30, threads=12)
+		# Aplica uma marca d'água em vídeos com imagens correspondentes.
+		for video in video_names:
+			for image in image_names:
+				if extension_validation(video, image):
+					video_file = os.path.join(video_path, video)
+					image_file = os.path.join(image_path, image)
+					video_name = os.path.splitext(video)[0]
+					image_name = os.path.splitext(image)[0]
+					try:
+						if video_name == image_name:
+							video_clip = VideoFileClip(video_file)
+							image_clip = ImageClip(image_file)
+							image_clip = image_clip.set_duration(video_clip.duration)
+							video_with_watermark = CompositeVideoClip([video_clip, image_clip.set_position(('right','top'))])
+							render_path = os.path.join(render_path, f"{video_name}_rendered.mp4")
+							video_with_watermark.write_videofile(render_path, codec='libx264', fps=30, threads=12)
+					except Exception as e:
+						print(f"Erro ao processar video {video_name}: {str(e)}")					
 
 def main():
 	# Definindo os caminhos para os diretórios de vídeo e imagem
@@ -56,7 +60,8 @@ def main():
 	image_path = os.path.abspath("watermark")
 	render_path = os.path.abspath("render")
 
-	directories_validation(video_path, image_path, render_path)
+	create_directories(video_path, image_path, render_path)
 	apply_watermark(video_path, image_path, render_path)
 
-main()
+if __name__ == "__main__":
+    main()
